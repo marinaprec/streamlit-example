8091efd82e215c63ab61c91fceaf0a688dd28743
marinaprec <58332885+marinaprec@users.noreply.github.com>
2024-03-19 09:48:10 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,8 +7,6 @@ import streamlit as st
 # !!Bienvenidos a nuestro predictor de Netflix!!!
 
 """
-
-import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
